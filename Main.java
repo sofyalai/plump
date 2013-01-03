acaf098499c9fba0b27531716cf7583686c0160b
sofyalai <sofyalai@gmail.com>
2013-01-03 23:26:01 +0100

    formated the code

--- Main.java
+++ Main.java
@@ -1,24 +1,19 @@
 
-
 import javax.swing.JFrame;
-
 
 import windows.MainMenuFrame;
 import windows.Splitpane;
 
-
-
 public class Main {
-	
-	public static void main (String[] args){
-	
-	
+
+	public static void main(String[] args) {
+
 		MainMenuFrame window1 = new MainMenuFrame();
 		window1.setTitle("Plump");
-		window1.setBounds(20, 20 ,500, 500);
+		window1.setBounds(20, 20, 500, 500);
 		window1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window1.setVisible(true);
-		//Splitpane frame1 = new Splitpane();
-	
-}
+		// Splitpane frame1 = new Splitpane();
+
+	}
 }
